7fff40af2c7b02c38cd11242a56b99df2ba703f2
Chanism <48588531+ykdy3951@users.noreply.github.com>
2024-10-01 12:36:04 +0900

    Title: 1이 되는 순간까지, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 241001/1이 되는 순간까지/until-the-moment-I-reach-one.py
+++ 241001/1이 되는 순간까지/until-the-moment-I-reach-one.py
@@ -1,10 +1,10 @@
 def solve(n) -> int:
     if n == 1:
-        return 1
+        return 0
     if n % 2:
-        cnt = solve(n // 2)
-    else:
         cnt = solve(n // 3)
+    else:
+        cnt = solve(n // 2)
     return 1 + cnt
 
 print(solve(int(input())))
