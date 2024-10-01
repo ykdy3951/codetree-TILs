64a816fa9f7a7c0ab457625fe5243794ba971793
Chanism <48588531+ykdy3951@users.noreply.github.com>
2024-10-01 13:26:40 +0900

    Title: 그 요일은, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241001/그 요일은/the-day-of-the-day.py
+++ 241001/그 요일은/the-day-of-the-day.py
@@ -1,11 +1,12 @@
 m1, d1, m2, d2 = map(int,input().split())
 dotk = input()
 
+t = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+
 l = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 for i in range(12):
     l[i+1] += l[i]
 
-days = l[m2 - 1] + d2 - (l[m1 - 1] + d1 + 4)
-
-print(days // 7 + 1)
+days = l[m2 - 1] + d2 - (l[m1 - 1] + d1 - 1 + t.index(dotk))
+print(days // 7 + 1 if days > 0 else 0)
