975acdb03d6f4f797d4c3b9f99a0d27e8166325d
Chanism <48588531+ykdy3951@users.noreply.github.com>
2024-10-01 13:59:24 +0900

    Title: 신기한 타일 뒤집기, Time: 120ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 241001/신기한 타일 뒤집기/strange-flipping-tiles.py
+++ 241001/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -1,15 +1,10 @@
 n = int(input())
-l = [0] * 101
+l = [0, 0]
 for _ in range(n):
     x, cnt = input().split()
     x = int(x)
     cnt = 1 if cnt == 'L' else 2
-    for i in range(x):
-        l[i] = cnt
     
-ans = [0, 0]
-for i in range(101):
-    if l[i] == 0:
-        break
-    ans[l[i]-1]+=1
-print(' '.join(map(str,ans)))
+    l[cnt-1] += x
+    l[2-cnt] = max(0, l[2-cnt]-x)
+print(l[0], l[1])
