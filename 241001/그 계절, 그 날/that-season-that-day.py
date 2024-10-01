63de75e73e03e2fe0fe824e6f9a157499604c716
Chanism <48588531+ykdy3951@users.noreply.github.com>
2024-10-01 11:49:09 +0900

    Title: 그 계절, 그 날, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241001/그 계절, 그 날/that-season-that-day.py
@@ -0,0 +1,33 @@
+def is_leap(y):
+    if y % 400 == 0:
+        return True
+    elif y % 100 == 0:
+        return False
+    elif y % 4 == 0:
+        return True
+    return False
+
+def is_valid_day(y, m, d):
+    if m > 12:
+        return False
+
+    if m == 2:
+        if is_leap(y):
+            return d < 30
+        else:
+            return d < 29
+    
+    elif m in [1, 3, 5, 7, 8, 10, 12]:
+        return d < 32
+
+    else:
+        return d < 31
+
+def solve(y, m, d):
+    if is_valid_day(y, m, d):
+        print(['Spring', 'Summer', 'Fall', 'Winter'][(m + 9) // 3])
+    else:
+        print(-1)
+
+y,m,d=map(int,input().split())
+solve(y, m, d)
