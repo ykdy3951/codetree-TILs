c37bd75f19dfa815da9dbd32455b94580f206e98
Chanism <48588531+ykdy3951@users.noreply.github.com>
2024-10-03 00:56:08 +0900

    Title: 특정 문자로 끝나는 문자열, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 241003/특정 문자로 끝나는 문자열/string-ending-with-specific-character.py
+++ 241003/특정 문자로 끝나는 문자열/string-ending-with-specific-character.py
@@ -1,2 +1,2 @@
-l=[input() for _ in range(10)];e=input()
-print('\n'.join(filter(lambda x: x[-1] == e, l)))
+l=[input() for _ in range(10)];e=input();l=list(filter(lambda x: x[-1] == e, l))
+print('\n'.join(l) if len(l) else 'None')
