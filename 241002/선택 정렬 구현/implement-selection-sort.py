21bbede73e8a37348b10e6274d46077c9b6f6210
Chanism <48588531+ykdy3951@users.noreply.github.com>
2024-10-02 22:23:34 +0900

    Title: 선택 정렬 구현, Time: 111ms, Memory: 28MB, Status: Passed - CodeTree

--- 241002/선택 정렬 구현/implement-selection-sort.py
+++ 241002/선택 정렬 구현/implement-selection-sort.py
@@ -1,6 +1,8 @@
 n=int(input());l=list(map(int,input().split()))
 for i in range(n-1):
     x=i
-    t=l.index(min(l[i:]))
-    l[i],l[t]=l[t],l[i]
+    for j in range(i+1,n):
+        if l[j] < l[x]:
+            x=j
+    l[x],l[i]=l[i],l[x]
 print(*l)
