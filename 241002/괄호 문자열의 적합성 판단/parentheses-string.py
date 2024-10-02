2e0fd452b871eb71c4c3782c0161a89f1ac4e94e
Chanism <48588531+ykdy3951@users.noreply.github.com>
2024-10-02 22:51:06 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 136ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 241002/괄호 문자열의 적합성 판단/parentheses-string.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+def solve(s : str) -> bool:
+    d = deque()
+    for i in s:
+        if i == '(':
+            d.append(i)
+        else:
+            if len(d):
+                return False
+            d.pop()
+    if len(d):
+        return False
+    return True
+
+print(['No','Yes'][solve(input())])
