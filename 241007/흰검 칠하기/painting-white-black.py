a9e33c1de1786207bc2499b1c3f26f92615c287c
Chanism <48588531+ykdy3951@users.noreply.github.com>
2024-10-07 18:06:34 +0900

    Title: 흰검 칠하기, Time: 184ms, Memory: 36MB, Status: Wrong Answer - CodeTree

--- 241007/흰검 칠하기/painting-white-black.py
+++ 241007/흰검 칠하기/painting-white-black.py
@@ -1,40 +1,28 @@
 N = int(input())
 CENTER = 100000
 t = 0
-l = []
-cnt = [0] * (2 * CENTER + 5)
+cnt = [[-1,0] for _ in range(2 * CENTER + 5)]
 for i in range(N):
     x, y = input().split()
     x = int(x)
 
     if y == 'L':
-        l.append([t-x+1, t])
+        for j in range(t, t-x, -1):
+            if cnt[j + CENTER][1] >= 3:
+                cnt[j + CENTER] = [2, 4]
+            else:
+                cnt[j + CENTER] = [0, cnt[j + CENTER][1] + 1]
         t = t-x+1
     else:
-        l.append([t, t+x-1])
+        for j in range(t, t+x):
+            if cnt[j + CENTER][1] >= 3:
+                cnt[j + CENTER] = [2, 4]
+            else:
+                cnt[j + CENTER] = [1, cnt[j + CENTER][1] + 1]
         t = t+x-1
 
-for i in l:
-    for j in range(i[0], i[1]+1):
-        cnt[j + CENTER] += 1
-
-ans = [0] *3
+ans = [0] * 3
 for i in range(2 * CENTER + 5):
-    if i - CENTER < 0:
-        if cnt[i] > 3:
-            ans[2] += 1
-        elif cnt[i] > 0:
-            ans[1 - (cnt[i] % 2)] += 1
-    elif i == CENTER:
-        if cnt[i] > 3:
-            ans[2] += 1
-        elif l[0][0]:
-            ans[1 - (cnt[i] % 2)] += 1
-        else:
-            ans[cnt[i] % 2] += 1
-    else:
-        if cnt[i] > 3:
-            ans[2] += 1
-        elif cnt[i] > 0:
-            ans[cnt[i] % 2] += 1
+    if cnt[i][0] >= 0:
+        ans[cnt[i][0]] += 1
 print(*ans)
