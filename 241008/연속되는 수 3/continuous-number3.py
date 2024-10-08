c16132d7dc8765bf3a746aba3b980e6545674b92
Chanism <48588531+ykdy3951@users.noreply.github.com>
2024-10-08 19:44:14 +0900

    Title: 연속되는 수 3, Time: 134ms, Memory: 73MB, Status: Passed - CodeTree

---
+++ 241008/연속되는 수 3/continuous-number3.py
@@ -0,0 +1,14 @@
+n=int(input())
+prev = -1
+cnt = 0
+ans = 0 
+for i in range(n):
+    x = int(input()) > 0
+    if x != prev:
+        ans = max(ans, cnt)
+        prev = x
+        cnt = 1
+    else:
+        cnt += 1
+
+print(max(ans, cnt))
