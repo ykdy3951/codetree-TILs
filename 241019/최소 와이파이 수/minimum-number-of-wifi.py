1b25448deff437bbcb4062dffa9f08ad2b7a08b1
Chanism <48588531+ykdy3951@users.noreply.github.com>
2024-10-19 22:46:35 +0900

    Title: 최소 와이파이 수, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 241019/최소 와이파이 수/minimum-number-of-wifi.py
+++ 241019/최소 와이파이 수/minimum-number-of-wifi.py
@@ -1,20 +1,17 @@
 n,m=map(int,input().split())
 l=list(map(int,input().split()))
 x=0
-st, val = 0, 0
+st, val = 0, -1
 
 while st < n:
     if l[st] == 1:
-        val +=1
+        val=st
     
-    if val == m:
+    if val != -1:
         x += 1
-        st += m + 1
-        val = 0
+        st += 2 * m
+        val = -1
 
     st += 1
-
-if val:
-    x += 1
 
 print(x)
