536a8e92245e1a181423e2760550e91778942d56
Chanism <48588531+ykdy3951@users.noreply.github.com>
2024-10-20 16:31:20 +0900

    Title: 전부 포함하는 선분, Time: 109ms, Memory: 28MB, Status: Passed - CodeTree

--- 241020/전부 포함하는 선분/all-inclusive-segment.py
+++ 241020/전부 포함하는 선분/all-inclusive-segment.py
@@ -4,4 +4,7 @@ l.sort()
 minX = [l[0][0], l[1][0]]
 l.sort(key=lambda x : -x[1])
 maxX = [l[0][1], l[1][1]]
-print(min(maxX[0] - minX[1], maxX[1]-minX[0]))
+if minX[0] == l[0][0]:
+    print(maxX[1] - minX[1])
+else:
+    print(min(maxX[0] - minX[1], maxX[1]-minX[0]))
