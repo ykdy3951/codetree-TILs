630fca78b00cd0a15a1647607e8ee93083d7b3de
Chanism <48588531+ykdy3951@users.noreply.github.com>
2024-10-31 13:57:13 +0900

    Title: 야바위, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241031/야바위/ya-rock.py
@@ -0,0 +1,15 @@
+n=int(input())
+x = [1, 2, 3]
+score = [0, 0, 0]
+for i in range(n):
+    a, b, c = map(int, input().split())
+    for i in range(3):
+        if a == x[i]:
+            x[i] = b
+        elif b == x[i]:
+            x[i] = a
+
+        if x[i] == c:
+            score[i] += 1
+
+print(score.index(max(score)) + 1)
