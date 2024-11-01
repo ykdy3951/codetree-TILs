f459d1c84e1c4326c13c21a7c115c2804e470594
Chanism <48588531+ykdy3951@users.noreply.github.com>
2024-11-01 14:02:00 +0900

    Title: 작은 구슬의 이동, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241101/작은 구슬의 이동/small-marble-movement.py
@@ -0,0 +1,37 @@
+n, t = map(int,input().split())
+r, c, d = input().split()
+r, c = int(r), int(c)
+
+D = {
+    'R' : [0, 1],
+    'U' : [1, 0],
+    'D' : [-1, 0],
+    'L' : [0, -1]
+}
+
+if d in 'LR':
+    while t:
+        if c == 1 or c == n:
+            d = list(D.keys())[3-list(D.keys()).index(d)]
+            t -= 1
+            if t == 0:
+                break
+        r += D[d][0]
+        c += D[d][1]
+        
+        t -= 1
+
+else:
+    while t:
+        if r == 1 or r == n:
+            d = list(D.keys())[3-list(D.keys()).index(d)]
+            t -= 1
+            if t == 0:
+                break
+
+        r += D[d][0]
+        c += D[d][1]
+        
+        t -= 1
+        
+print(r, c)
