627452357e129ad391fe7e3456d52c24967c4e74
Chanism <48588531+ykdy3951@users.noreply.github.com>
2024-11-11 18:23:50 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 64ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 241111/빙빙 돌며 사각형 채우기/snail-alphabet-square.py
@@ -0,0 +1,22 @@
+def snail_array(n, m):
+    array = [[0] * m for _ in range(n)]
+    num = 0
+    x, y = 0, 0
+    dx, dy = 0, 1
+
+    while num <= n * m - 1:
+        array[x][y] = chr(num % 27 + 65)
+        num += 1
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < n and 0 <= ny < m and array[nx][ny] == 0:
+            x, y = nx, ny
+        else:
+            dx, dy = dy, -dx
+            x, y = x + dx, y + dy
+
+    return array
+
+n, m = map(int, input().split())
+result = snail_array(n, m)
+for row in result:
+    print(*row)
