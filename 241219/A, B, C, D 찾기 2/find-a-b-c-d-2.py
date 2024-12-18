b44d8f59fef02277480ed91d08533253514b165d
Chanism <48588531+ykdy3951@users.noreply.github.com>
2024-12-19 00:48:23 +0900

    Title: A, B, C, D 찾기 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241219/A, B, C, D 찾기 2/find-a-b-c-d-2.py
@@ -0,0 +1,23 @@
+def read_input():
+    import sys
+    input = sys.stdin.read
+    data = list(map(int, input().strip().split()))
+    return data
+
+def find_abcd(values):
+    values.sort()
+
+    A = values[0]
+    B = values[1]
+    C = values[2]
+    D = values[-1] - A - B - C
+
+    return A, B, C, D
+
+def main():
+    values = read_input()
+    A, B, C, D = find_abcd(values)
+    print(A, B, C, D)
+
+if __name__ == "__main__":
+    main()
