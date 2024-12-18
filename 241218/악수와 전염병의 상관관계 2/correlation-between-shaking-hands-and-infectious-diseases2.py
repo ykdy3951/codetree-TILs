4ade2eda6af4c3ba4b7f306b6e3317518c2a0aba
Chanism <48588531+ykdy3951@users.noreply.github.com>
2024-12-18 20:51:15 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241218/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 241218/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -1,5 +1,6 @@
 N, K, P, T = map(int, input().split())
 P -= 1
+
 handshakes = []
 for i in range(1, T + 1):
     t, x, y = map(int, input().split())
@@ -10,7 +11,8 @@ infected = [0] * N
 transmit_count = [0] * N
 infected[P] = 1
 transmit_count[P] = K
-for t, x, y in handshakes:
+
+for _, x, y in handshakes:
     if infected[x] and transmit_count[x] > 0:
         if not infected[y]:
             infected[y] = 1
@@ -22,4 +24,5 @@ for t, x, y in handshakes:
             infected[x] = 1
             transmit_count[x] = K
         transmit_count[y] -= 1
+
 print(''.join(map(str, infected)))
